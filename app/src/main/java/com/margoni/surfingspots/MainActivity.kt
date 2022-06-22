7b9697b2e82d34013258123c507fde545ed772a3
Nicola Margoni <margoni.nicola@gmail.com>
2022-06-22 11:20:03 +0200

    creates project

---
+++ app/src/main/java/com/margoni/surfingspots/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.margoni.surfingspots
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
