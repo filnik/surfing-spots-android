8725b13f33c19a631da0cecf38b0e0d950460853
Nicola Margoni <margoni.nicola@gmail.com>
2022-06-27 16:50:26 +0200

    adds random sequence generator test coverage

---
+++ app/src/test/java/com/margoni/surfingspots/data/RandomSequenceGeneratorImplTest.kt
@@ -0,0 +1,28 @@
+package com.margoni.surfingspots.data
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class RandomSequenceGeneratorImplTest {
+    private val generator = RandomSequenceGeneratorImpl()
+    @Test
+    fun `random sequence generator`() {
+        val sequences = (0 until 100).map { generator.generate(3) }
+
+        sequences.forEach { assertThat(it).contains(0, 1, 2) }
+        assertThat(sequences.distinct().size).isGreaterThan(1)
+    }
+
+    @Test
+    fun `next random sequence not repeat last of old sequence and first of new sequence`() {
+        val sequences = (0..100).map { generator.generate(3) }
+
+        val listOfPairSequences = sequences.map { Pair(it, generator.nextSequence(it)) }
+
+        listOfPairSequences.forEach {
+            assertThat(it.first).containsAll(it.second)
+            assertThat(it.first).doesNotContainSequence(it.second)
+            assertThat(it.first.last()).isNotEqualTo(it.second.first())
+        }
+    }
+}
