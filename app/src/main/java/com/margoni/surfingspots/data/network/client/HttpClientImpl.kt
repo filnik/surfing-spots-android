f8ab45fa6c2c5f2897b263a4c7b7a0e168985f40
Nicola Margoni <margoni.nicola@gmail.com>
2022-06-27 15:09:11 +0200

    refactors http client

--- app/src/main/java/com/margoni/surfingspots/data/network/client/HttpClientImpl.kt
+++ app/src/main/java/com/margoni/surfingspots/data/network/client/HttpClientImpl.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.net.HttpURLConnection
+import java.net.HttpURLConnection.HTTP_OK
 import java.net.URL
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
@@ -13,18 +14,27 @@ class HttpClientImpl(
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : HttpClient {
 
-    override suspend fun get(url: String, params: Map<String, String>): String = withContext(ioDispatcher) {
-        try {
-            val url = if (params.isEmpty()) url else "$url?${urlEncode(params)}"
+    override suspend fun get(url: String, params: Map<String, String>): String =
+        withContext(ioDispatcher) scope@{
+            try {
+                val url = if (params.isEmpty()) url else "$url?${urlEncode(params)}"
 
-            val connection = URL(url).openConnection() as HttpURLConnection
+                val connection = URL(url).openConnection() as HttpURLConnection
 
-            connection.requestMethod = HttpMethod.GET
+                connection.requestMethod = HttpMethod.GET
 
-            return@withContext connection.inputStream?.bufferedReader()?.readText()!!
-        } catch (t: Throwable) {
-            throw NetworkException()
+                if (connection.responseCode != HTTP_OK) {
+                    throw NetworkException()
+                }
+
+                return@scope connection.responseMessage()
+            } catch (t: Throwable) {
+                throw NetworkException()
+            }
         }
+
+    private fun HttpURLConnection.responseMessage(): String {
+        return inputStream?.bufferedReader()?.readText()!!
     }
 
     private fun urlEncode(params: Map<String, String>): String {
